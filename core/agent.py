8ce0e69eb208a2372022321aa84f90003211c761
journalanonymous25 <icseanonymous25@gmail.com>
2025-02-04 15:35:30 +0700

    Update agent.py with claude setting

--- core/agent.py
+++ core/agent.py
@@ -1,5 +1,6 @@
 from error import InvalidOpenAIFinishReasonError, FailedOpenAIRequestError
 from openai import OpenAI
+from anthropic import Anthropic
 from config import GLOBAL_CONFIG
 import json
 
@@ -8,7 +9,7 @@ class APIUsageManager:
     response_time = {}
 
     @classmethod
-    def record_usage(cls, module, model, usage):
+    def record_usage(cls, module, client_option, model, usage):
         if module not in cls.usage:
             cls.usage[module] = {
                 'model': model,
@@ -16,9 +17,15 @@ class APIUsageManager:
                 'completion_tokens': 0,
                 'total_tokens': 0,
             }
-        cls.usage[module]['prompt_tokens'] += usage.prompt_tokens
-        cls.usage[module]['completion_tokens'] += usage.completion_tokens
-        cls.usage[module]['total_tokens'] += usage.total_tokens
+        if (client_option == "openai"):
+          print("Usage: ", usage)
+          cls.usage[module]['prompt_tokens'] += usage.prompt_tokens
+          cls.usage[module]['completion_tokens'] += usage.completion_tokens
+          cls.usage[module]['total_tokens'] += usage.total_tokens
+        elif (client_option == "anthropic"):
+          cls.usage[module]['prompt_tokens'] += usage.input_tokens
+          cls.usage[module]['completion_tokens'] += usage.output_tokens
+          cls.usage[module]['total_tokens'] += usage.input_tokens + usage.output_tokens
 
     @classmethod
     def record_response_time(cls, model, response_time):
@@ -40,34 +47,59 @@ class APIUsageManager:
         cls.response_time = {}
 
 class Agent:
-  def ask(prompt, model=None, temperature = 0, module = "all", json_response=True):
-    client = OpenAI(api_key = GLOBAL_CONFIG['agent']['key'])
+  def ask(prompt, client_option= "openai", model=None, temperature = 0, module = "all", json_response=True):
     model = model if model else GLOBAL_CONFIG['agent']['model']
     response = None
-    try:
-      if json_response:
-        response = client.chat.completions.create(
-          model= model,
-          messages=prompt,
-          temperature=temperature,
-          response_format={ 'type': 'json_object' },
-          seed=0
-        )
+    if client_option == "openai":
+      client = OpenAI(api_key = GLOBAL_CONFIG['agent']['key'])
+      try:
+        if json_response:
+          response = client.chat.completions.create(
+            model= model,
+            messages=prompt,
+            temperature=temperature,
+            response_format={ 'type': 'json_object' },
+            seed=0
+          )
+        else:
+          response = client.chat.completions.create(
+            model= model,
+            messages=prompt,
+            temperature=temperature,
+            seed=0
+          )
+      except Exception as e:
+        raise FailedOpenAIRequestError(e)
+      
+      if (response.choices[0].finish_reason != "stop"):
+        raise InvalidOpenAIFinishReasonError()
+      
+      APIUsageManager.record_usage(module, client_option, model, response.usage)
+      if (json_response):
+        return json.loads(response.choices[0].message.content)
       else:
-        response = client.chat.completions.create(
-          model= model,
-          messages=prompt,
-          temperature=temperature,
-          seed=0
-        )
-    except Exception as e:
-      raise FailedOpenAIRequestError(e)
-    
-    if (response.choices[0].finish_reason != "stop"):
-      raise InvalidOpenAIFinishReasonError()
-    
-    APIUsageManager.record_usage(module, model, response.usage)
-    if (json_response):
-      return json.loads(response.choices[0].message.content)
-    else:
-      return response.choices[0].message.content
+        return response.choices[0].message.content
+    elif client_option == "anthropic":
+      client = Anthropic(api_key = GLOBAL_CONFIG['agent']['key'])
+      try:
+        if json_response:
+          response = client.messages.create(
+            max_tokens=1000,
+            model= model,
+            temperature=temperature,
+            system=prompt[0]["content"],
+            messages=[
+                prompt[1]
+            ],
+          )
+      except Exception as e:
+        raise FailedOpenAIRequestError(e)
+      print(response.content[0].text)
+      
+      APIUsageManager.record_usage(module, client_option, model, response.usage)
+      if (json_response):
+        return json.loads(response.content[0].text)
+      else:
+        return response.content[0].text
+    elif client == "llama":
+      pass
